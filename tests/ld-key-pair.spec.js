091cca4607d43db69799547c457a8d85643280d3
Matthew Collier <collier.matthew@gmail.com>
2019-01-25 18:49:52 -0500

    Add APIs and tests.

--- tests/ld-key-pair.spec.js
+++ tests/ld-key-pair.spec.js
@@ -1,9 +1,14 @@
 'use strict';
 
 const chai = require('chai');
-const {util: {binary: {base58}}} = require('node-forge');
+const {
+  md: {sha256},
+  pki: {getPublicKeyFingerprint, publicKeyFromPem},
+  util: {binary: {base58}}
+} = require('node-forge');
 const multibase = require('multibase');
 const multicodec = require('multicodec');
+const multihashes = require('multihashes');
 chai.use(require('dirty-chai'));
 chai.should();
 
@@ -31,7 +36,9 @@ describe('LDKeyPair', () => {
     describe('fingerprint', () => {
       it('should create an Ed25519 key fingerprint', async () => {
         const keyPair = await Ed25519KeyPair.generate();
-        expect(typeof keyPair.fingerprint()).to.equal('string');
+        const fingerprint = keyPair.fingerprint();
+        fingerprint.should.be.a('string');
+        fingerprint.startsWith('z').should.be.true;
       });
       it('should be properly multicodec encoded', async () => {
         const keyPair = await Ed25519KeyPair.generate();
@@ -43,6 +50,19 @@ describe('LDKeyPair', () => {
         const encodedPubkey = base58.encode(pubkeyBytes);
         encodedPubkey.should.equal(keyPair.publicKeyBase58);
         expect(typeof keyPair.fingerprint()).to.equal('string');
+      });
+    });
+
+    describe('verify fingerprint', () => {
+      it('should verify a valid fingerprint', async () => {
+        const keyPair = await Ed25519KeyPair.generate();
+        const fingerprint = keyPair.fingerprint();
+        const result = keyPair.verifyFingerprint(fingerprint);
+        expect(result).to.exist;
+        result.should.be.an('object');
+        expect(result.valid).to.exist;
+        result.valid.should.be.a('boolean');
+        result.valid.should.be.true;
       });
     });
 
@@ -123,7 +143,50 @@ describe('LDKeyPair', () => {
     describe('fingerprint', () => {
       it('should create an RSA key fingerprint', async () => {
         const keyPair = await RSAKeyPair.generate();
-        expect(typeof keyPair.fingerprint()).to.equal('string');
+        const fingerprint = keyPair.fingerprint();
+        fingerprint.should.be.a('string');
+        fingerprint.startsWith('z').should.be.true;
+      });
+      it('should be properly multicodec encoded', async () => {
+        const keyPair = await RSAKeyPair.generate();
+        const fingerprint = keyPair.fingerprint();
+        const mcPubkeyBytes = multibase.decode(fingerprint);
+
+        // FIXME: multicodec doesn't know about 0x5d encoding yet
+        let error;
+        let mcType;
+        try {
+          mcType = multicodec.getCodec(mcPubkeyBytes);
+        } catch(e) {
+          error = e;
+        }
+        expect(mcType).to.be.undefined;
+        error.message.should.equal('Code `0x5d` not found');
+
+        const multihashBytes = multicodec.rmPrefix(mcPubkeyBytes);
+        mcType = multicodec.getCodec(multihashBytes);
+        mcType.should.equal('sha2-256');
+        // send hash, including prefix to multihashes.decode
+        const hashHex = multihashes.decode(multihashBytes)
+          .digest.toString('hex');
+        // compute the fingerprint directly from the keyPair
+        const fingerprintHex = getPublicKeyFingerprint(
+          publicKeyFromPem(keyPair.publicKeyPem), {md: sha256.create()})
+          .toHex();
+        hashHex.should.equal(fingerprintHex);
+      });
+    });
+
+    describe('verify fingerprint', () => {
+      it('should verify a valid fingerprint', async () => {
+        const keyPair = await RSAKeyPair.generate();
+        const fingerprint = keyPair.fingerprint();
+        const result = keyPair.verifyFingerprint(fingerprint);
+        expect(result).to.exist;
+        result.should.be.an('object');
+        expect(result.valid).to.exist;
+        result.valid.should.be.a('boolean');
+        result.valid.should.be.true;
       });
     });
 
