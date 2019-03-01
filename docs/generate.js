0c1dee6bd2ed9f8851464050227192c94cd522b6
Matthew Collier <collier.matthew@gmail.com>
2019-03-01 09:31:11 -0500

    Fix generation script.

--- docs/generate.js
+++ docs/generate.js
@@ -5,12 +5,12 @@ const jsdoc2md = require('jsdoc-to-markdown');
 const fs = require('fs');
 
 const template = fs.readFileSync('./docs/template.hbs', 'utf-8');
-const opFactory = file => ({template, files: `./lib/${file}`});
+const opFactory = file => ({template, files: `./lib/${file.name}`});
 
 // we only generate docs for files which end with keypair or index.
 const docs = /(keypair|index).js/i;
-const files = fs.readdirSync('./lib/', {withFileTypes: true})
-  .filter(p => docs.test(p));
+const files = fs.readdirSync('./lib', {withFileTypes: true})
+  .filter(p => docs.test(p.name));
 files.forEach(filePath => {
   const options = opFactory(filePath);
   const doc = jsdoc2md.renderSync(options);
@@ -19,6 +19,6 @@ files.forEach(filePath => {
   if(!doc || doc.length < template.length) {
     return false;
   }
-  const docFileName = './docs/' + filePath.replace(/.js/, '.md');
+  const docFileName = './docs/' + filePath.name.replace(/.js/, '.md');
   fs.writeFileSync(docFileName, doc);
 });
