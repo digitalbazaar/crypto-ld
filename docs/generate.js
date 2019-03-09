b9e2a0bc0b65a48c1be4825dbe655ec5c3ad5c83
andrewjones <ajones@digitalbazaar.com>
2019-03-08 19:05:13 -0600

    Add a check for .name in docs/generate.js.

--- docs/generate.js
+++ docs/generate.js
@@ -5,12 +5,12 @@ const jsdoc2md = require('jsdoc-to-markdown');
 const fs = require('fs');
 
 const template = fs.readFileSync('./docs/template.hbs', 'utf-8');
-const opFactory = file => ({template, files: `./lib/${file.name}`});
+const opFactory = file => ({template, files: `./lib/${file.name || file}`});
 
 // we only generate docs for files which end with keypair or index.
 const docs = /(keypair|index).js/i;
 const files = fs.readdirSync('./lib', {withFileTypes: true})
-  .filter(p => docs.test(p.name));
+  .filter(p => docs.test(p || p.name));
 files.forEach(filePath => {
   const options = opFactory(filePath);
   const doc = jsdoc2md.renderSync(options);
@@ -19,6 +19,7 @@ files.forEach(filePath => {
   if(!doc || doc.length < template.length) {
     return false;
   }
-  const docFileName = './docs/' + filePath.name.replace(/.js/, '.md');
+  const fpath = filePath.name || filePath;
+  const docFileName = './docs/' + fpath.replace(/.js/, '.md');
   fs.writeFileSync(docFileName, doc);
 });
