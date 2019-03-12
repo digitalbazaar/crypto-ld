e6f6aefaf590eff6cf8af448c4f81a55123d2238
andrewjones <ajones@digitalbazaar.com>
2019-03-12 10:45:47 -0600

    Remove checks for file.name.

--- docs/generate.js
+++ docs/generate.js
@@ -5,12 +5,12 @@ const jsdoc2md = require('jsdoc-to-markdown');
 const fs = require('fs');
 
 const template = fs.readFileSync('./docs/template.hbs', 'utf-8');
-const opFactory = file => ({template, files: `./lib/${file.name || file}`});
+const opFactory = file => ({template, files: `./lib/${file}`});
 
 // we only generate docs for files which end with keypair or index.
 const docs = /(keypair|index).js/i;
 const files = fs.readdirSync('./lib')
-  .filter(p => docs.test(p || p.name));
+  .filter(p => docs.test(p));
 files.forEach(filePath => {
   const options = opFactory(filePath);
   const doc = jsdoc2md.renderSync(options);
@@ -19,7 +19,7 @@ files.forEach(filePath => {
   if(!doc || doc.length < template.length) {
     return false;
   }
-  const fpath = filePath.name || filePath;
+  const fpath = filePath;
   const docFileName = './docs/' + fpath.replace(/.js/, '.md');
   fs.writeFileSync(docFileName, doc);
 });
